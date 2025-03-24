1648783ef4efff1eb1d24121be8a25b55f581215
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2025-03-24 00:30:58 -0600

    add sp in utilities

--- backend/backend.Utilities/Static/StoredProcedures.cs
+++ backend/backend.Utilities/Static/StoredProcedures.cs
@@ -8,28 +8,110 @@ namespace backend.Utilities.Static;
 public class SP
 {
     #region User
-    public const string SpCreateAppUser = "VETFRIENDS.PkgAppUser.SpCreateAppUser";
-    public const string SpDeleteAppUser = "VETFRIENDS.PkgAppUser.SpDeleteAppUser";
-    public const string SpUpdateAppUser = "VETFRIENDS.PkgAppUser.SpUpdateAppUser";
+    public const string SpCreateAppUser = "PkgAppUser.SpCreateAppUser";
+    public const string SpDeleteAppUser = "PkgAppUser.SpDeleteAppUser";
+    public const string SpUpdateAppUser = "PkgAppUser.SpUpdateAppUser";
     #endregion
+
     #region Client
     public const string SpCreateClient = "PkgClient.SpCreateClient";
     public const string SpDeleteClient = "PkgClient.SpDeleteClient";
     public const string SpUpdateClient = "PkgClient.SpUpdateClient";
     #endregion
+
     #region Medic
     public const string SpCreateMedic = "PkgMedic.SpCreateMedic";
     public const string SpDeleteMedic = "PkgMedic.SpDeleteMedic";
     public const string SpUpdateMedic = "PkgMedic.SpUpdateMedic";
     #endregion
+
     #region Pet
     public const string SpCreatePet = "PkgPet.SpCreatePet";
     public const string SpDeletePet = "PkgPet.SpDeletePet";
     public const string SpUpdatePet = "PkgPet.SpUpdatePet";
     #endregion
+
     #region Vaccine
     public const string SpCreateVaccine = "PkgVaccine.SpCreateVaccine";
     public const string SpDeleteVaccine = "PkgVaccine.SpDeleteVaccine";
     public const string SpUpdateVaccine = "PkgVaccine.SpUpdateVaccine";
+    #endregion
+
+    #region AppliedVaccine
+    public const string SpCreateAppliedVaccine = "PkgAppliedVaccine.SpCreateAppliedVaccine";
+    public const string SpDeleteAppliedVaccine = "PkgAppliedVaccine.SpDeleteAppliedVaccine";
+    public const string SpUpdateAppliedVaccine = "PkgAppliedVaccine.SpUpdateAppliedVaccine";
+    #endregion
+
+    #region Appointment
+    public const string SpCreateAppointment = "PkgAppointment.SpCreateAppointment";
+    public const string SpDeleteAppointment = "PkgAppointment.SpDeleteAppointment";
+    public const string SpUpdateAppointment = "PkgAppointment.SpUpdateAppointment";
+    #endregion
+
+    #region AppointmentDetail
+    public const string SpCreateAppointmentDetail = "PkgAppointmentDetail.SpCreateAppointmentDetail";
+    public const string SpDeleteAppointmentDetail = "PkgAppointmentDetail.SpDeleteAppointmentDetail";
+    public const string SpUpdateAppointmentDetail = "PkgAppointmentDetail.SpUpdateAppointmentDetail";
+    #endregion
+
+    #region Inventory
+    public const string SpCreateInventory = "PkgInventory.SpCreateInventory";
+    public const string SpDeleteInventory = "PkgInventory.SpDeleteInventory";
+    public const string SpUpdateInventory = "PkgInventory.SpUpdateInventory";
+    #endregion
+
+    #region Payment
+    public const string SpCreatePayment = "PkgPayment.SpCreatePayment";
+    public const string SpDeletePayment = "PkgPayment.SpDeletePayment";
+    public const string SpUpdatePayment = "PkgPayment.SpUpdatePayment";
+    #endregion
+
+    #region Product
+    public const string SpCreateProduct = "PkgProduct.SpCreateProduct";
+    public const string SpDeleteProduct = "PkgProduct.SpDeleteProduct";
+    public const string SpUpdateProduct = "PkgProduct.SpUpdateProduct";
+    #endregion
+
+    #region ProductCategory
+    public const string SpCreateProductCategory = "PkgProductCategory.SpCreateProductCategory";
+    public const string SpDeleteProductCategory = "PkgProductCategory.SpDeleteProductCategory";
+    public const string SpUpdateProductCategory = "PkgProductCategory.SpUpdateProductCategory";
+    #endregion
+
+    #region ProductCategoryRelation
+    public const string SpCreateProductCategoryRelation = "PkgProductCategoryRelation.SpCreateProductCategoryRelation";
+    public const string SpDeleteProductCategoryRelation = "PkgProductCategoryRelation.SpDeleteProductCategoryRelation";
+    public const string SpUpdateProductCategoryRelation = "PkgProductCategoryRelation.SpUpdateProductCategoryRelation";
+    #endregion
+
+    #region PurchaseOrden
+    public const string SpCreatePurchaseOrden = "PkgPurchaseOrden.SpCreatePurchaseOrden";
+    public const string SpDeletePurchaseOrden = "PkgPurchaseOrden.SpDeletePurchaseOrden";
+    public const string SpUpdatePurchaseOrden = "PkgPurchaseOrden.SpUpdatePurchaseOrden";
+    #endregion
+
+    #region PurchaseOrdenDetail
+    public const string SpCreatePurchaseOrdenDetail = "PkgPurchaseOrdenDetail.SpCreatePurchaseOrdenDetail";
+    public const string SpDeletePurchaseOrdenDetail = "PkgPurchaseOrdenDetail.SpDeletePurchaseOrdenDetail";
+    public const string SpUpdatePurchaseOrdenDetail = "PkgPurchaseOrdenDetail.SpUpdatePurchaseOrdenDetail";
+    #endregion
+
+    #region Sale
+    public const string SpCreateSale = "PkgSale.SpCreateSale";
+    public const string SpDeleteSale = "PkgSale.SpDeleteSale";
+    public const string SpUpdateSale = "PkgSale.SpUpdateSale";
+    #endregion
+
+    #region SaleDetail
+    public const string SpCreateSaleDetail = "PkgSaleDetail.SpCreateSaleDetail";
+    public const string SpDeleteSaleDetail = "PkgSaleDetail.SpDeleteSaleDetail";
+    public const string SpUpdateSaleDetail = "PkgSaleDetail.SpUpdateSaleDetail";
+    #endregion
+
+    #region Supplier
+    public const string SpCreateSupplier = "PkgSupplier.SpCreateSupplier";
+    public const string SpDeleteSupplier = "PkgSupplier.SpDeleteSupplier";
+    public const string SpUpdateSupplier = "PkgSupplier.SpUpdateSupplier";
     #endregion
 }
